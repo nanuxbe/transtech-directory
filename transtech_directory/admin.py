176d416f797676e99f44b0b694c772b41d7f9625
Justin Wing Chung Hui <wingchung@justinhui.co.uk>
2015-09-21 11:01:58 +0100

    Added basic admin for all models

--- transtech_directory/admin.py
+++ transtech_directory/admin.py
@@ -1,8 +1,23 @@
-from django.contrib import admin
+﻿from django.contrib import admin
 
-from .models import Directory
+from .models import Category, Directory, ContactInfo, Address
 
 
 @admin.register(Directory)
 class DirectoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ContactInfo)
+class ContactInfoAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
     pass
