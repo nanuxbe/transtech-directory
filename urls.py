f4f5d6617c3fe7fe234b955b7ee927951ca804b4
Leila <leila@levit.com>
2015-09-21 15:36:39 +0200

    table index created

--- urls.py
+++ urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
 
     url(r'^$', TemplateView.as_view(template_name='transtech_directory/index.html'), name='home'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^directory/$', DirectoryListView.as_view(), name='directory')
+    url(r'^directory/$', DirectoryListView.as_view(), name='directory'),
 ]
