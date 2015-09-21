25e8fa25d43392781873bc8993dd618c6a44355b
Emmanuelle Delescolle <emma@levit.be>
2015-09-21 12:50:11 +0200

    added tranlatable field labels + missing value field on contact info

--- transtech_directory/models.py
+++ transtech_directory/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import ugettext as _
 
 from django_countries.fields import CountryField
 
@@ -12,32 +13,33 @@ CONTACT_TYPES = (
 
 class Category(models.Model):
 
-    name = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100)
+    name = models.CharField(_('Category name'), max_length=100)
+    slug = models.SlugField(_('Slug'), max_length=100)
 
 
 class Directory(models.Model):
 
-    service_provider = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255)
-    service_category = models.ManyToManyField(Category)
-    link = models.URLField(blank=True, null=True)
+    service_provider = models.CharField(_('Service provider name'), max_length=255)
+    slug = models.SlugField(_('slug'), max_length=255)
+    service_category = models.ManyToManyField(Category, verbose_name=_('Categories'))
+    link = models.URLField(_('Link'), blank=True, null=True)
 
 
 class ContactInfo(models.Model):
 
-    directory = models.ForeignKey(Directory, related_name='contacts')
-    type = models.CharField(max_length=5, choices=CONTACT_TYPES)
+    directory = models.ForeignKey(Directory, verbose_name=_('Service provider'), related_name='contacts')
+    type = models.CharField(_('Type'), max_length=5, choices=CONTACT_TYPES)
+    value = models.CharField(_('Value'), max_length=400)
 
 
 class Address(models.Model):
 
-    directory = models.ForeignKey(Directory, related_name='addresses')
-    street = models.CharField(max_length=255)
-    street2 = models.CharField(max_length=255, blank=True, null=True)
-    city = models.CharField(max_length=100)
-    postal_code = models.CharField(max_length=10, blank=True, null=True)
-    country = CountryField()
+    directory = models.ForeignKey(Directory, verbose_name=('Service provider'), related_name='addresses')
+    street = models.CharField(_('Street address'), max_length=255)
+    street2 = models.CharField(_('Second line'), max_length=255, blank=True, null=True)
+    city = models.CharField(_('City'), max_length=100)
+    postal_code = models.CharField(_('postal_code'), max_length=10, blank=True, null=True)
+    country = CountryField(verbose_name=_('Country'))
 
-    longitude = models.FloatField(blank=True, null=True)
-    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(_('Longitude'), blank=True, null=True)
+    latitude = models.FloatField(_('Latitude'), blank=True, null=True)
