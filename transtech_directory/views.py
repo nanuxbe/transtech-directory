6e259923ad1cec475f43ed184e7326122573b9cc
Justin Wing Chung Hui <wingchung@justinhui.co.uk>
2015-09-21 15:09:09 +0100

    Added directory form

--- transtech_directory/views.py
+++ transtech_directory/views.py
@@ -1,7 +1,11 @@
-from django.views.generic import ListView
-
+﻿from django.views.generic import ListView
+from django.shortcuts import render
+from .forms import DirectoryForm
 from .models import Directory
 
 
 class DirectoryListView(ListView):
     model = Directory
+
+def directory_form(request):
+    return render(request, 'directory_form.html', {'form': DirectoryForm()})
--- urls.py
+++ urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import include, url
+﻿from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
 from django_comments import urls as comments_urls
 
 from transtech_directory.views import DirectoryListView
+import transtech_directory.views
 
 # from transtech_directory.views import HomeView
 
@@ -18,4 +19,5 @@ urlpatterns = [
     url(r'^directory/$', DirectoryListView.as_view(), name='directory'),
     url(r'^comments/', include(comments_urls)),
     url(r'^ratings/', include('star_ratings.urls', namespace='ratings', app_name='ratings')),
+    url(r'^directory/new/$', transtech_directory.views.directory_form, name='directory_form'),
 ]
